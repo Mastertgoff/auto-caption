930c3c37af0cdf965b11ef7d068d1d4c02f5ddc0
Mastertgoff <121784621+Mastertgoff@users.noreply.github.com>
2023-03-03 18:06:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    DB_URL = os.environ.get("DATABASE_URL", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5861684593:AAHWwARn74ZZFhjSjOBWeBm5GA61PAA2x48")
+    API_ID = int(os.environ.get("API_ID", 18302370))
+    API_HASH = os.environ.get("API_HASH", "03c2cced4dea9b1e96dce87558dd2381")
+    DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Mst:Mst@cluster0.g3uxpl0.mongodb.net/?retryWrites=true&w=majority")
